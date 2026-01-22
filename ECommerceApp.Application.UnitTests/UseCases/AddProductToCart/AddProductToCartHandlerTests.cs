4fb4f8838f9d7780dfe3ba5e17128227e01a5791
Milica Radenkovic <mr20210088@student.fon.bg.ac.rs>
2026-01-22 10:53:55 +0100

    AddProductToCartHandlerTests

--- ECommerceApp.Application.UnitTests/UseCases/AddProductToCart/AddProductToCartHandlerTests.cs
+++ ECommerceApp.Application.UnitTests/UseCases/AddProductToCart/AddProductToCartHandlerTests.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿using AutoFixture.Xunit2;
+using ECommerceApp.Application.IRepository;
+using ECommerceApp.Application.IServices;
+using ECommerceApp.Application.Services;
+using ECommerceApp.Application.UseCases.Commands.AddProductToCart;
+using ECommerceApp.Domain.Entities;
+using FluentAssertions;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +14,88 @@ using System.Threading.Tasks;
 
 namespace ECommerceApp.Application.UnitTests.UseCases.AddProductToCart
 {
-    internal class AddProductToCartHandlerTests
+    public class AddProductToCartHandlerTests
     {
+        [Theory]
+        [AutoMoqInlineData]
+        public async Task Handle_ValidRequest_AddProductToCart(
+            [Frozen] Mock<IProductRepository> productRepositoryMock,
+            [Frozen] Mock<IShoppingCartRepository> shoppingCartRepositoryMock,
+            [Frozen] Mock<StockAvailability> stockAvailabilityMock,
+            ProductEntity product,
+            AddProductToCartRequest request,
+            AddProductToCartHandler sut
+            )
+        {
+            productRepositoryMock
+                .Setup(x => x.GetById(request.ProductId))
+                .ReturnsAsync(product);
+
+            stockAvailabilityMock
+                .Setup(x => x.IsQuantityAvailable(
+                    product.Quantity.Value,
+                    request.ProductId,
+                    request.Quantity))
+                .ReturnsAsync(true);
+
+            var result = await sut.Handle(request, default);
+
+            result.IsSuccessful.Should().BeTrue();
+
+            shoppingCartRepositoryMock.Verify(
+                x => x.Add(It.Is<CartItemEntity>(item =>
+                    item.ProductId == request.ProductId &&
+                    item.Quantity.Value == request.Quantity)), 
+                Times.Once());
+        }
+
+        [Theory]
+        [AutoMoqInlineData]
+        public async Task Handle_ProductNotFound_ReturnsFailure(
+            [Frozen] Mock<IProductRepository> productRepositoryMock,
+            [Frozen] Mock<IShoppingCartRepository> shoppingCartRepositoryMock,
+            AddProductToCartRequest request,
+            AddProductToCartHandler sut
+            )
+        {
+            productRepositoryMock
+                .Setup(x => x.GetById(request.ProductId))
+                .ReturnsAsync((ProductEntity?)null);
+
+            var result = await sut.Handle(request, default);
+
+            result.IsSuccessful.Should().BeFalse();
+
+            shoppingCartRepositoryMock.Verify(x => x.Add(It.IsAny<CartItemEntity>()), Times.Never());
+        }
+
+        [Theory]
+        [AutoMoqInlineData]
+        public async Task Handle_NotEnoughStock_ReturnsFailure(
+            [Frozen] Mock<IProductRepository> productRepositoryMock,
+            [Frozen] Mock<IShoppingCartRepository> shoppingCartRepositoryMock,
+            [Frozen] Mock<StockAvailability> stockAvailabilityMock,
+            ProductEntity product,
+            AddProductToCartRequest request,
+            AddProductToCartHandler sut
+            )
+        {
+            productRepositoryMock
+                .Setup(x => x.GetById(request.ProductId))
+                .ReturnsAsync(product);
+
+            stockAvailabilityMock
+                .Setup(x => x.IsQuantityAvailable(
+                    product.Quantity.Value,
+                    request.ProductId,
+                    request.Quantity))
+                .ReturnsAsync(false);
+
+            var result = await sut.Handle(request, default);
+
+            result.IsSuccessful.Should().BeFalse();
+
+            shoppingCartRepositoryMock.Verify(x => x.Add(It.IsAny<CartItemEntity>()), Times.Never());
+        }
     }
 }
