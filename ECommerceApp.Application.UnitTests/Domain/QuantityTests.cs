208665003ea58c72644fb2f7e826bbec912ee7e4
Milica Radenkovic <mr20210088@student.fon.bg.ac.rs>
2026-06-29 16:29:53 +0200

    Add unit tests and mutation testing

---
+++ ECommerceApp.Application.UnitTests/Domain/QuantityTests.cs
@@ -0,0 +1,39 @@
+﻿using ECommerceApp.Domain.ValueObjects;
+using FluentAssertions;
+
+namespace ECommerceApp.Application.UnitTests.Domain
+{
+    public class QuantityTests
+    {
+        [Fact]
+        public void FromInt_WhenValueIsPositive_CreatesQuantity()
+        {
+            var quantity = Quantity.FromInt(5);
+
+            quantity.Value.Should().Be(5);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        [InlineData(-10)]
+        public void FromInt_WhenValueIsZeroOrNegative_ThrowsArgumentException(int value)
+        {
+            Action action = () => Quantity.FromInt(value);
+
+            action.Should().Throw<ArgumentException>()
+                .WithMessage("*Quantity must be greater than zero*");
+        }
+
+        [Fact]
+        public void Add_WhenTwoQuantitiesAreAdded_ReturnsQuantityWithSum()
+        {
+            var first = Quantity.FromInt(2);
+            var second = Quantity.FromInt(3);
+
+            var result = first.Add(second);
+
+            result.Value.Should().Be(5);
+        }
+    }
+}
---
+++ ECommerceApp.Application.UnitTests/Services/DiscountTests.cs
@@ -0,0 +1,49 @@
+﻿using ECommerceApp.Application.Services;
+using FluentAssertions;
+
+namespace ECommerceApp.Application.UnitTests.Services
+{
+    public class DiscountTests
+    {
+        [Theory]
+        [InlineData("0641234560", 300)]
+        [InlineData("0641234562", 200)]
+        [InlineData("0641234564", 200)]
+        [InlineData("0641234566", 200)]
+        [InlineData("0641234568", 200)]
+        [InlineData("0641234561", 100)]
+        [InlineData("0641234563", 100)]
+        [InlineData("0641234565", 100)]
+        [InlineData("0641234567", 100)]
+        [InlineData("0641234569", 100)]
+        public void Calculate_WhenOrderIsBetween16And17_ReturnsDiscountBasedOnLastDigit(
+            string phoneNumber,
+            decimal expectedDiscount)
+        {
+            var discount = new Discount();
+
+            var result = discount.Calculate(
+                1000m,
+                phoneNumber,
+                new DateTime(2026, 6, 28, 16, 30, 0));
+
+            result.Should().Be(expectedDiscount);
+        }
+
+        [Theory]
+        [InlineData(15)]
+        [InlineData(17)]
+        [InlineData(18)]
+        public void Calculate_WhenOrderIsOutsideDiscountPeriod_ReturnsZero(int hour)
+        {
+            var discount = new Discount();
+
+            var result = discount.Calculate(
+                1000m,
+                "0641234560",
+                new DateTime(2026, 6, 28, hour, 0, 0));
+
+            result.Should().Be(0);
+        }
+    }
+}
